1176606f6a355c228c4019453c31e20975489ec2
cristobal <cris_to515@hotmail.com>
2025-11-21 21:54:47 -0300

    parte 1 lista

--- estructuras.c
+++ estructuras.c
@@ -1,39 +1,73 @@
-//linked list 
+// linked list
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
-typedef struct Node
+typedef struct node
 {
    int data;
-   struct node* next;
-}node;
+   struct node *next;
+} node;
 
 typedef struct queue
 {
    node *head;
    node *tail;
    int size;
-}queue;
+} queue;
 
-void create_queue(){
-   queue* q= malloc(sizeof(queue));
-   q->head=NULL;
-   q->tail=NULL;
-   q->size=0;
+queue *create_queue()
+{
+   queue *q = malloc(sizeof(queue));
+   q->head = NULL;
+   q->tail = NULL;
+   q->size = 0;
    return q;
 }
 
-
-void enqueue(queue* queue ,int memutilization){
-   node* newnode= malloc(sizeof(queue));
+void enqueue(queue *queue, int memutilization)
+{
+   node *newnode = malloc(sizeof(node));
    newnode->data = memutilization;
-   newnode->next=NULL;
+   newnode->next = NULL;
 
-   if(queue->tail==NULL){
-      queue->head=newnode;
-      queue->tail=newnode;
+   if (queue->tail == NULL)
+   {
+      queue->head = newnode;
+      queue->tail = newnode;
    }
    else
    {
-      queue->tail->next=newnode;
-      queue->tail=newnode;
+      queue->tail->next = newnode;
+      queue->tail = newnode;
    }
+   queue->size++;
+}
+int deque(queue *queue)
+{
+   if (queue->head == NULL)
+   {
+      return -1;
+   }
+
+   node *temp = queue->head;
+   int value = queue->head->data;
+   queue->head = queue->head->next;
+
+   if (queue->head == NULL)
+   {
+      queue->tail = NULL;
+   }
+
+   queue->size--;
+   return value;
+}
+
+int peek(queue *queue)
+{
+   if (queue->head->data == NULL)
+   {
+      return -1;
+   }
+   return queue->head->data;
 }
--- maint3.c
+++ maint3.c
@@ -3,65 +3,62 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
-//tamaño de procesos definido aca
-int random_between(int memory_size,int  min, double max){ 
-    srand(time(NULL));
 
-    // 2. Definir el rango deseado
-    //double min = 1.5;
-    //double max = 2.2;
-
-    // 3. Generar un número aleatorio en el rango
-    //  - rand() / RAND_MAX genera un número entre 0.0 y 1.0
-    //  - Se multiplica por la diferencia entre max y min
-    //  - Se suma el valor mínimo para desplazar el rango
+#include "estructuras.c"
+// tamaño de procesos definido aca
+int random_between(int memory_size, double min, double max)
+{
+    
     double numero_aleatorio = min + (rand() / (double)RAND_MAX) * (max - min);
-    int random_memory = memory_size * numero_aleatorio ;
+    int random_memory = memory_size * numero_aleatorio;
     return random_memory;
 }
 
-int page_usage(int proceso, int tam_page){
+int page_usage(int proceso, int tam_page)
+{
     int page_usage = proceso / tam_page;
     return page_usage;
 }
 
-int main(){ 
-    //tamaño de memoria a ram
-    int memory_size ;
+int main()
+{
+    
+    
+    // tamaño de memoria a ram
+    int memory_size;
     printf("Ingrese el tamaño de memoria a reservar: ");
     scanf("%d", &memory_size);
 
-    //memoria virtual para el swap
-    int virtual_memory = random_between(memory_size,1.5,4.5);
+    // memoria virtual para el swap
+    int virtual_memory = random_between(memory_size, 1.5, 4.5);
 
-    // tamaño de la pagina 
+    // tamaño de la pagina
     int tam_page;
     printf("Ingrese el tamaño de la página: ");
     scanf("%d", &tam_page);
+    
 
-    //crear los procesos cada 2 segundos
+    // crear los procesos cada 2 segundos
     bool lleno = false;
-    queue* procesos = create_queue();
+    queue *procesos = create_queue();
+
+        //para el aleatorio
+        srand(time(NULL));
+
     while (!lleno)
     {
         sleep(2);
-        int proceso_size = random_between(memory_size,0.1,(double)memory_size);
-        queue procesos; 
-        procesos.enqueue(procesos,proceso_size);
+        int proceso_size = random_between(memory_size, 0.1, (double)memory_size);
+        queue *procesos;
+        enqueue(procesos, proceso_size);
 
-        
-
-/*
-        int proceso[];
-        int i=0;i++;
-        //tamaño del proceso no puede ser mayor a la ram fisica
-        proceso[i]=crear_proceso();
-*/        
-        
+        /*
+                int proceso[];
+                int i=0;i++;
+                //tamaño del proceso no puede ser mayor a la ram fisica
+                proceso[i]=crear_proceso();
+        */
     }
-    
-
-    
 
     return 0;
 }
